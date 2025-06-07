f81c48cebc08ab7c3df7aed721ef47714d7616af
Daniil <dadyarri@gmail.com>
2025-06-08 00:43:19 +0300

    add missing stuff about dices and react

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [icon(), react()],
   site: "https://cod.dadyarri.ru",
 });
