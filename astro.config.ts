71c08a871c1f935cfcbd0bdd77f4ee030c1a7db6
Daniil <dadyarri@gmail.com>
2025-06-08 14:29:20 +0300

    drop react support; rewrite rollable-dice in vanilla astro

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
-import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  integrations: [icon(), react(), mdx()],
+  integrations: [icon(), mdx()],
   site: "https://cod.dadyarri.ru",
 });
