14284265a41f088be68ea47a30aab5e388ed485c
Daniil <dadyarri@gmail.com>
2025-06-08 03:28:13 +0300

    add mdx support

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,9 @@ import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
-  integrations: [icon(), react()],
+  integrations: [icon(), react(), mdx()],
   site: "https://cod.dadyarri.ru",
 });
